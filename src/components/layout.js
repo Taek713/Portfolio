96417e8bf1c0f083e66b06e7cd84df138b21bb6f
Taek713 <tae.kim147@gmail.com>
2022-01-18 10:54:52 -0500

    Changing names

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,9 @@ const Layout = ({ children }) => {
           //   marginTop: `2rem`,
           // }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.github.com/Taek713">Tae Kim</a>
         </footer>
       </div>
     </>
